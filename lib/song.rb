9de4f6a69374c2ecdf71aeb636382511919cc9ce
Crystal Norman <cnorm88@gmail.com>
2020-10-08 18:33:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 # creating songs given each filename and sending the artist's name (a string) to the Artist class
 # relies on the Artist instances to build associations.
-
+require 'pry'
 class Song
   
   attr_accessor :name, :artist
