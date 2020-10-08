75ee08e54992694acbf212e76decf5bb89572bc4
Crystal Norman <cnorm88@gmail.com>
2020-10-08 22:38:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,5 +29,9 @@ class Song
   # def artist_name(name)
   #   Artist.find(name) ? Artist.find(name) : self.new(name)
   # end 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
   
 end
