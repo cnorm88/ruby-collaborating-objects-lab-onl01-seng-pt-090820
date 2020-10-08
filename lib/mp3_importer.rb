dcfa362346f9b50446dccd7239808c7359ede284
Crystal Norman <cnorm88@gmail.com>
2020-10-08 21:41:25 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,7 +10,7 @@ class MP3Importer
   end
   
   def files
-    Dir.glob("#{path}/*.mp3").map do |file|
+    Dir.glob("#{path}/*.mp3").collect do |file|
       file.gsub("./spec/fixtures/mp3s/", " ")
     end
   end
