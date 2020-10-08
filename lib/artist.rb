d75bd899d08f6871e8c7369aef68b3e651146983
Crystal Norman <cnorm88@gmail.com>
2020-10-08 18:46:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,7 +28,7 @@ class Artist
     song.artist = self
   end
 
-  def self.find_or_create_by_name()
+  def self.find_or_create_by_name(name)
     # Find the artist instance that has that name or create one if it doesn't exist. Either way, the return value of the method will be an instance of an artist with the name attribute filled out
     self.find(name) ? self.find(name) : self.new(name)
   end
